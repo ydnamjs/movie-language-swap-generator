e7c05824ad4aeac1c0d6e68664ffe1ffc296c2c8
michael <ydna.mjs@gmail.com>
2023-09-23 21:33:48 -0400

    Basic python script that generates random points to swap the movie to different languages. TODO: Make it so that swaps cant collide

---
+++ shrek.py
@@ -0,0 +1,76 @@
+import math
+import random
+
+# CONSTANTS
+MOVIE_SECONDS = 90 * 60
+NUM_SWAPS = 5
+SWAP_MIN_SECONDS = 30
+SWAP_MAX_SECONDS = 90
+SWAP_LANGUAGES = ["Spanish", "French"]
+
+# HELPER FUNCTIONS
+def generateSwap() -> int:
+
+    return random.randrange(1, MOVIE_SECONDS)
+
+def generateEnglishReturn(swapTime: int) -> int:
+
+    return swapTime + random.randrange(SWAP_MIN_SECONDS, SWAP_MAX_SECONDS)
+
+def secondsToTime(seconds: int):
+    
+    time_hours = math.floor((seconds / 60) / 60)
+
+    seconds = seconds - (time_hours * 60 * 60)
+
+    time_minutes = math.floor((seconds / 60))
+
+    seconds = seconds - (time_minutes * 60)
+
+    return [time_hours, time_minutes, seconds]
+
+def generateSwaps():
+
+    times = []
+
+    for i in range(0, NUM_SWAPS):
+
+        newSwapTime = generateSwap()
+
+        times.append(secondsToTime(newSwapTime))
+        times.append(secondsToTime(generateEnglishReturn(newSwapTime)))
+
+    return times
+
+def printTime(time, newLanguage: str):
+
+    hours = time[0]
+    minutes = time[1]
+    seconds = time[2]
+
+    print(
+    ("0" + hours.__str__() if hours < 10 else hours.__str__()) + ":" +
+    ("0" + minutes.__str__() if minutes < 10 else minutes.__str__()) + ":" +
+    ("0" + seconds.__str__() if seconds < 10 else seconds.__str__()) + 
+    " -> Switch to " + newLanguage)
+
+def printTimes(times):
+
+    isReturnToEnglish = False
+
+    for i in times:
+
+        if (isReturnToEnglish):
+            printTime(i, "English")
+            isReturnToEnglish = False
+            print("")
+        else:
+            printTime(i, SWAP_LANGUAGES[random.randint(0, 1)])
+            isReturnToEnglish = True
+
+# MAIN
+def main():
+    
+    printTimes(generateSwaps())
+
+main()
